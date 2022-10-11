b7d12728e9eba4f85b87d12a963502b04fff3d0e
Ugwuanyi TobeChukwu <39131739+amaify@users.noreply.github.com>
2022-10-11 13:13:49 +0100

    Project Setup

---
+++ app/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import { Meta } from "../components/shared/Meta";
+
+const Home: NextPage = () => {
+	return (
+		<div>
+			<Meta />
+			<h1>Audiophile App</h1>
+		</div>
+	);
+};
+
+export default Home;
