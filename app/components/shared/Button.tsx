7feb981a223cb4e4b46903cd606838addf2cbbe4
Ugwuanyi TobeChukwu <39131739+amaify@users.noreply.github.com>
2022-10-18 17:18:13 +0100

    Added the Assets folder and created the button component.

---
+++ app/components/shared/Button.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { clsx } from "clsx";
+import ArrowRightIcon from "../../assets/shared/desktop/icon-arrow-right.svg";
+
+interface Props {
+	btnType: number;
+	btnText: string;
+}
+
+const Button = ({ btnText, btnType }: Props) => {
+	const buttonStyle = clsx(
+		"[ phile-btn ]",
+		btnType === 1 && "[ phile-btn-1 ]",
+		btnType === 2 && "[ phile-btn-2 ]",
+		btnType === 3 && "[ phile-btn-3 ]"
+	);
+	return (
+		<Link href="#">
+			<a className={buttonStyle}>
+				<span>{btnText}</span>
+				{btnType === 3 && (
+					<span>
+						<Image
+							src={ArrowRightIcon}
+							alt="Arrow Right"
+							width={"5px"}
+							height={"10px"}
+						/>
+					</span>
+				)}
+			</a>
+		</Link>
+	);
+};
+
+export default Button;
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from "next";
 import { Meta } from "../components/shared/Meta";
+import Button from "../components/shared/Button";
 
 const Home: NextPage = () => {
 	return (
 		<div>
 			<Meta />
-			<h1>Audiophile App</h1>
+			<h1 className="text-blue-600">Audiophile App</h1>
+			<Button btnType={3} btnText="shop" />
 		</div>
 	);
 };
