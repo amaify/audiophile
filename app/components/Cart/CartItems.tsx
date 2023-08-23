aeed8db4c97c83d2c7b0140b7269fc3ea6e0aa18
amaify2 <tobe@trufin.io>
2023-08-23 13:34:14 +0100

    feat: improved the cart component

--- app/components/Cart/CartItems.tsx
+++ app/components/Cart/CartItems.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useSelector, useDispatch } from "react-redux";
+import clsx from "clsx";
 import { removeAllItems, selectCart } from "@/store/reducers/cartReducer";
 import { formatPrice, itemPriceSum } from "@/components/util/utils";
 import IncOrDecCartItems from "./IncOrDecCartItems";
@@ -11,8 +12,10 @@ const CartItems = () => {
   const dispatch = useDispatch();
   const { cart } = useSelector(selectCart);
 
+  const checkoutDisabled = cart.length === 0;
+
   return (
-    <div className="bg-white flex flex-col py-[31px] pr-[31px] pl-[33px] rounded-lg w-[37.7rem] max-h-[48.8rem] overflow-auto absolute top-[10rem] right-0 z-30">
+    <div className="bg-white flex flex-col py-[3.1rem] pr-[3.1rem] pl-[3.3rem] rounded-lg w-[37.7rem] max-h-[48.8rem] overflow-auto absolute top-[10rem] right-0 z-30">
       <div className="mb-[3.1rem] flex">
         <h6 className="mr-auto [ heading-6 ] font-bold">{`cart (${cart.length})`}</h6>
         <button
@@ -28,7 +31,13 @@ const CartItems = () => {
           cart.map((item) => (
             <div className="flex items-center" key={item.id}>
               <div className="flex items-center gap-[16px] mr-auto">
-                <Image src={item.cartImage} alt={item.title} width={64} height={64} className="rounded-[8px]" />
+                <Image
+                  src={item.cartImage}
+                  alt={item.title}
+                  width={64}
+                  height={64}
+                  className="rounded-lg w-auto h-auto"
+                />
                 <div>
                   <p className="text-[1.5rem] text-black font-bold uppercase">{item.title}</p>
                   <p className="text-[1.4rem] text-black font-bold opacity-50">{formatPrice(item.price)}</p>
@@ -51,10 +60,15 @@ const CartItems = () => {
         <p className="mr-auto [ body-text ] text-black opacity-50 uppercase">total</p>
         <p className="[ body-text ] text-black">{formatPrice(itemPriceSum(cart))}</p>
       </div>
-      <Link href="/checkout">
-        <span className="bg-primary hover:bg-primaryHover text-white text-center w-full py-[15px] font-bold [ phile-btn ]">
-          checkout
-        </span>
+      <Link
+        href="/checkout"
+        aria-disabled={checkoutDisabled}
+        className={clsx(
+          "bg-primary hover:bg-primaryHover text-white text-center w-full py-[15px] font-bold [ phile-btn ]",
+          checkoutDisabled && "bg-primary/40 hover:bg-primary/40 cursor-not-allowed pointer-events-none"
+        )}
+      >
+        checkout
       </Link>
     </div>
   );
--- app/components/Cart/IncOrDecCartItems.tsx
+++ app/components/Cart/IncOrDecCartItems.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-nested-ternary */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { TrashIcon } from "@heroicons/react/24/solid";
@@ -18,9 +16,23 @@ const IncOrDecCartItems = ({ itemQuantity, itemId, isCartVisible, addedStyle }: 
   const dispatch = useDispatch();
   const itemCount = useSelector(selectItemCount);
 
+  const minusText = <span>&#8722;</span>;
+  let decrementCountText = minusText;
+
+  if (isCartVisible) {
+    decrementCountText =
+      itemQuantity > 1 ? (
+        minusText
+      ) : (
+        <span onClick={() => dispatch(removeFromCart(itemId ?? ""))}>
+          <TrashIcon className="w-7 h-7 text-black hover:fill-primary" />
+        </span>
+      );
+  }
+
   return (
     <div className={clsx("bg-darkGrey flex items-center justify-center px-[15px]", addedStyle)}>
-      <p
+      <button
         className="font-bold text-black text-[2em] opacity-25 leading-[18px] mr-auto hover:text-primary hover:opacity-100 hover:cursor-pointer"
         onClick={() =>
           !isCartVisible
@@ -28,23 +40,15 @@ const IncOrDecCartItems = ({ itemQuantity, itemId, isCartVisible, addedStyle }: 
             : itemQuantity > 1 && dispatch(decrementCartCount(itemId ?? ""))
         }
       >
-        {!isCartVisible ? (
-          <span>&#8722;</span>
-        ) : itemQuantity > 1 ? (
-          <span>&#8722;</span>
-        ) : (
-          <span onClick={() => dispatch(removeFromCart(itemId ?? ""))}>
-            <TrashIcon className="w-7 h-7 text-black hover:fill-primary" />
-          </span>
-        )}
-      </p>
+        {decrementCountText}
+      </button>
       <p className="mr-auto text-[1.3rem] font-bold leading-[18px] tracking-[1px]">{itemQuantity}</p>
-      <p
+      <button
         className="font-bold text-black text-[2em] opacity-25 leading-[18px] hover:text-primary hover:opacity-100 hover:cursor-pointer"
         onClick={() => (!isCartVisible ? dispatch(incrementCount()) : dispatch(incrementCartCount(itemId ?? "")))}
       >
         &#43;
-      </p>
+      </button>
     </div>
   );
 };
