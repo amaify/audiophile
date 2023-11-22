6887967ebabb8eddf7e9f11a8c947ca6cfa657a7
amaify2 <tobe@trufin.io>
2023-11-22 10:41:58 +0000

    chore: setup react query

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,22 +1,29 @@
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { ApolloProvider } from "@apollo/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
 import client from "@/helpers/apolloClient";
 import Notification from "@/components/Notification";
 import { store, persistor } from "../store/store";
 import "../styles/main.css";
 
+const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <Provider store={store}>
-        <PersistGate persistor={persistor} loading={null}>
-          <Notification />
-          <Component {...pageProps} />
-        </PersistGate>
-      </Provider>
-    </ApolloProvider>
+    <QueryClientProvider client={queryClient}>
+      <ApolloProvider client={client}>
+        <Provider store={store}>
+          <PersistGate persistor={persistor} loading={null}>
+            <Notification />
+            <Component {...pageProps} />
+          </PersistGate>
+        </Provider>
+      </ApolloProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
