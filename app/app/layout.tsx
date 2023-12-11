c301118d59ef2984272d10ee67f2ea102527f59e
amaify2 <tobe@trufin.io>
2023-12-11 09:07:33 +0000

    chore: added meta description

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -4,6 +4,8 @@ import AppProvider from "./app-provider";
 
 export const metadata: Metadata = {
   title: "Audiophile",
+  description:
+    "Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio.",
   icons: {
     icon: "/favicon-32x32.png",
     shortcut: "/favicon-32x32.png",
