cb949eec711f7052fbdc19a9b7515b3494b38e2b
amaify2 <tobe@trufin.io>
2023-12-13 15:28:07 +0000

    feat: Removed subfolders

--- helpers/ServiceClient.ts
+++ helpers/ServiceClient.ts
@@ -1,7 +1,7 @@
 interface QueryData<U> {
   query: string;
   variables?: { [key: string]: U };
-  cache?: "no-store" | "no-cache" | "default";
+  cache?: RequestCache;
 }
 
 export async function fetchHygraphData<T, U = void>({
@@ -10,7 +10,7 @@ export async function fetchHygraphData<T, U = void>({
   cache = "default"
 }: QueryData<U>): Promise<{ data: T }> {
   const uri = "https://api-eu-west-2.hygraph.com/v2/clq3jr60y9uqz01uh468vh3q2/master";
-  const options = {
+  const options: RequestInit = {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
