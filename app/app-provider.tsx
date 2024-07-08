9fa73faa67c6f7e5ce4bb5543d945b0e6f3697f2
amaify2 <tobe@trufin.io>
2024-07-08 13:25:05 +0100

    chore: packages upgrade

--- app/app-provider.tsx
+++ app/app-provider.tsx
@@ -4,10 +4,10 @@ import { type ReactNode } from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { store, persistor } from "@/store/store";
 import Notification from "@/components/shared/Notification";
 import "../styles/main.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 export default function AppProvider({ children }: { children: ReactNode }) {
